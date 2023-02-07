8907bcaf6b102f6504e27f895c28b9a3add86f97
Michael Tu <michaeltu12345@gmail.com>
2023-02-07 17:40:10 -0500

    added reserved word map, fixed return types

--- src/edu/ufl/cise/plcsp23/Scanner.java
+++ src/edu/ufl/cise/plcsp23/Scanner.java
@@ -1,5 +1,6 @@
 package edu.ufl.cise.plcsp23;
 import java.util.Arrays;
+import java.util.HashMap;
 
 import edu.ufl.cise.plcsp23.IToken.Kind;
 
@@ -8,12 +9,39 @@ public class Scanner implements IScanner {
     final char[] inputChars;
     int pos;
     char ch;
+    HashMap<String, Kind> reservedWords = new HashMap<String, Kind>();
 
     public Scanner (String input) {
         this.input = input;
         this.inputChars = Arrays.copyOf(input.toCharArray(), input.length()+1);
         pos = 0;
         ch = inputChars[pos];
+        reservedWords.put("image", Kind.RES_image);
+        reservedWords.put("pixel", Kind.RES_pixel);
+        reservedWords.put("int", Kind.RES_int);
+        reservedWords.put("string", Kind.RES_string);
+        reservedWords.put("void", Kind.RES_void);
+        reservedWords.put("nil", Kind.RES_nil);
+        reservedWords.put("load", Kind.RES_load);
+        reservedWords.put("display", Kind.RES_display);
+        reservedWords.put("write", Kind.RES_write);
+        reservedWords.put("x", Kind.RES_x);
+        reservedWords.put("y", Kind.RES_y);
+        reservedWords.put("a", Kind.RES_a);
+        reservedWords.put("r", Kind.RES_r);
+        reservedWords.put("X", Kind.RES_X);
+        reservedWords.put("Y", Kind.RES_Y);
+        reservedWords.put("Z", Kind.RES_Z);
+        reservedWords.put("x_cart", Kind.RES_x_cart);
+        reservedWords.put("y_cart", Kind.RES_y_cart);
+        reservedWords.put("a_polar", Kind.RES_a_polar);
+        reservedWords.put("r_polar", Kind.RES_r_polar);
+        reservedWords.put("rand", Kind.RES_rand);
+        reservedWords.put("sin", Kind.RES_sin);
+        reservedWords.put("cos", Kind.RES_cos);
+        reservedWords.put("atan", Kind.RES_atan);
+        reservedWords.put("if", Kind.RES_if);
+        reservedWords.put("while", Kind.RES_while);
     }
 
     @Override
@@ -30,7 +58,7 @@ public class Scanner implements IScanner {
         IN_OPERATOR
     }
 
-    private Token scanToken() throws LexicalException {
+    private IToken scanToken() throws LexicalException {
         State state = State.START;
         int index = -1;
         while(true) {
@@ -56,7 +84,7 @@ public class Scanner implements IScanner {
                     }
                     if (ch == '0') {
                         pos++;
-                        return new Token(Kind.NUM_LIT, pos-1, 1, inputChars);
+                        return new NumLitToken(pos-1, 1, inputChars);
                     }
                     if (checkDig == true) {
                         state = State.IN_NUM_LIT;
