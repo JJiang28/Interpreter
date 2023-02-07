3d19edcf6351f4fcfc66d7e0539d3f538e35d3f9
JJiang28 <jiangjason28@gmail.com>
2023-02-07 14:03:23 -0500

    Accounting for certain cases

--- src/edu/ufl/cise/plcsp23/Scanner.java
+++ src/edu/ufl/cise/plcsp23/Scanner.java
@@ -1,6 +1,8 @@
 package edu.ufl.cise.plcsp23;
 import java.util.Arrays;
 
+import edu.ufl.cise.plcsp23.IToken.Kind;
+
 public class Scanner implements IScanner {
 	final String input;
     final char[] inputChars;
@@ -13,4 +15,66 @@ public class Scanner implements IScanner {
         pos = 0;
         ch = inputChars[pos];
     }
+
+    @Override
+    public IToken next() throws LexicalException {
+        return scanToken();
+    }
+
+    private enum State {
+        START,
+        IN_INDENT,
+        IN_NUM_LIT,
+        IN_STRING_LIT,
+        IN_RESERVED,
+        IN_OPERATOR,
+        IN_WHITESPACE
+    }
+
+    private Token scanToken() throws LexicalException {
+        State state = State.START;
+        int index = -1;
+        while(true) {
+            switch(state) {
+                case START -> {
+                    index = pos;
+                    boolean checkDig = isDigit(ch);
+                    boolean checkLet = isLetter(ch);
+                    boolean isIdent = isIdentStart(ch);
+                    boolean isOp = isOper(ch);
+                    if(isIdent == true) {
+                        return new Token(Kind.IDENT, pos, index, inputChars);
+                    }
+                }
+                case IN_INDENT -> {}
+                case IN_NUM_LIT -> {}
+                case IN_STRING_LIT -> {}
+                case IN_RESERVED -> {}
+                case IN_OPERATOR -> {}
+                case IN_WHITESPACE -> {}
+                default -> { throw new UnsupportedOperationException("Bug");}
+            }
+           
+        }
+
+    }
+
+    private boolean isDigit(int c) {
+        return '0' <= c && c <= '9';
+     }
+     private boolean isLetter(int c) {
+        return ('A' <= c && c <= 'Z') || ('a' <= c && c <= 'z');
+     }
+     private boolean isIdentStart(int c) {
+        return isLetter(c) || (c == '$') || (c == '_');
+     }
+     private void error(String message) throws LexicalException{
+        throw new LexicalException("Error at pos " + pos + ": " + message); 
+     }
+     private boolean isOper(char c) {
+        switch(c) {
+            case '.', ',', '?', ':', '(', ')', '<', '>', '[', ']', '{', '}', '=', '!', '&', '|', '+', '-', '*', '/', '%' -> {return true;}
+            default -> {return false;}
+        }
+     }
 }
