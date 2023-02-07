3bdf888c9e3a83910bf3a56fc3d694353d2604f9
JJiang28 <jiangjason28@gmail.com>
2023-02-07 10:12:33 -0500

    Token Class and some other stuff

---
+++ src/edu/ufl/cise/plcsp23/Token.java
@@ -0,0 +1,31 @@
+package edu.ufl.cise.plcsp23;
+
+public class Token implements IToken {
+    final Kind kind;
+    final int pos;
+    final int length;
+    final char[] source;
+
+    public Token(Kind kind_, int pos_, int length_, char[] source_) {
+        super();
+        this.kind = kind_;
+        this.pos = pos_;
+        this.length = length_;
+        this.source = source_;
+    }
+
+    @Override
+    public Kind getKind() {
+        return this.kind;
+    }
+
+    @Override
+    public String getTokenString() {
+        return new String(this.source);
+    }
+
+    @Override
+    public SourceLocation getSourceLocation() {
+        return this.getSourceLocation();
+    }
+}
