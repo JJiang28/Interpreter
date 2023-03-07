189eb5c1a8d5924dd30c36fa2f8b7b5cff031686
Michael Tu <michaeltu12345@gmail.com>
2023-03-07 15:15:12 -0500

    fixed case 14

--- src/edu/ufl/cise/plcsp23/Assignment3Test_starter.java
+++ src/edu/ufl/cise/plcsp23/Assignment3Test_starter.java
@@ -929,7 +929,7 @@ class Assignment3Test_starter {
 					xx = 22;
 					}
 					""";
-			assertThrows(SyntaxException.class, () -> {
+			assertThrows(PLCException.class, () -> {
 				@SuppressWarnings("unused")
 				AST ast = getAST(input);
 			});
